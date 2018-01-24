54854086f4ddceb6d92103cf90db448f5dc7beab
Malcolm Foster <malcolmfoster@Malcolms-MacBook-Air.local>
2018-01-24 16:07:30 -0700

    ready for deployment

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "encoding/json"
+	"net/http"
+	"github.com/go-chi/chi"
+)
+
+type turtle struct {
+    Name string `json:"name"`
+    Age int `json:"age"`
+    Color string `json:"color"`
+}
+
+func main() {
+	router := chi.NewRouter()
+    donatelo := turtle{Name: "Donatelo", Age: 100, Color: "purple"}
+	router.Get("/", func(writer http.ResponseWriter, router *http.Request) {
+        writer.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(writer).Encode(donatelo)
+	})
+	http.ListenAndServe(":3000", router)
+}
